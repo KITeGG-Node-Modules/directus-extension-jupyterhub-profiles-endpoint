57940dfc57399d28b70d6320e06fc108c15fd9c1
Anton <antonymousfeedback@gmail.com>
2024-03-25 11:47:16 +0100

    Show all reserved profiles

--- src/index.js
+++ src/index.js
@@ -52,16 +52,18 @@ export default {
 
 			const today = DateTime.now().toFormat('yyyy-MM-dd')
 			try {
-				const [profileReservation] = await usersService.knex('gpu_reservations as gr')
+				const reservations = await usersService.knex('gpu_reservations as gr')
 					.select('gr.gpu')
 					.innerJoin('gpu_reservations_directus_users as grdu',
 						'gr.id', '=', 'grdu.gpu_reservations_id')
 					.where('grdu.directus_users_id', user.id)
 					.andWhere('gr.start', '<=', today)
 					.andWhere('gr.end', '>', today)
-				const reservedProfile = profiles.find(profile => profile.slug === profileReservation?.gpu)
 				allowedProfiles = profiles.filter(profile => profile.slug === 'no-gpu')
-				if (reservedProfile) allowedProfiles.push(reservedProfile)
+				for (const profileReservation of reservations) {
+					const reservedProfile = profiles.find(profile => profile.slug === profileReservation?.gpu)
+					if (reservedProfile) allowedProfiles.push(reservedProfile)
+				}
 			} catch (err) {
 				console.error('Failed to fetch reservations:', err.message)
 			}
